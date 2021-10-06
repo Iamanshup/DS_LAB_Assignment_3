91ad383e30a9cdb61f77f681289d93cedadcc48e
iamanshup <raj.anshu66@gmail.com>
2021-10-06 23:48:42 +0530

    removed threads and added some getter functions

--- BST.h
+++ BST.h
@@ -9,7 +9,6 @@ class Node
 {
 public:
   Node *left, *right;
-  bool lthread, rthread;
   int val;
 
 public:
@@ -18,19 +17,6 @@ public:
   {
     this->val = val;
     left = right = NULL;
-    lthread = rthread = false;
-  }
-};
-
-// class needed for PrintTree() function
-class DoubleTrees
-{
-public:
-  Node *left, *right;
-  DoubleTrees()
-  {
-    left = NULL;
-    right = NULL;
   }
 };
 
@@ -57,16 +43,19 @@ public:
   BST(BST &bst);
 
   void insert(int x);
-  Node *search(int x);
+  bool search(int x);
   void deleteKey(int x);
   Node *successor(Node *node);
   Node *predecessor(Node *node);
   void printTree();
   void printTreeHelper(const Node *node, ofstream &fout);
   void split(int k);
-  DoubleTrees *splitHelper(Node *node, int k);
   int BST_Height();
   double BST_Average_Height_Of_Nodes();
+  int get_number_of_rotations_during_insertion();
+  int get_number_of_rotations_during_deletion();
+  int get_number_of_comparisons_during_insertion();
+  int get_number_of_comparisons_during_deletion();
 };
 
 // default constructor
@@ -97,11 +86,8 @@ void BST::copyConstructorHelper(const Node *node)
     return;
   insert(node->val);
 
-  if (!node->lthread)
-    copyConstructorHelper(node->left);
-
-  if (!node->rthread)
-    copyConstructorHelper(node->right);
+  copyConstructorHelper(node->left);
+  copyConstructorHelper(node->right);
 }
 
 // insert function
@@ -121,19 +107,11 @@ void BST::insert(int x)
 
     if (x < cur->val)
     {
-      // go to left child if exists
-      if (!cur->lthread)
-        cur = cur->left;
-      else
-        break;
+      cur = cur->left;
     }
     else
     {
-      // go to right child if exists
-      if (!cur->rthread)
-        cur = cur->right;
-      else
-        break;
+      cur = cur->right;
     }
   }
 
@@ -145,24 +123,16 @@ void BST::insert(int x)
   }
   else if (x < par->val) // insert as left child
   {
-    node->lthread = node->rthread = true;
-    node->left = par->left;
-    node->right = par;
     par->left = node;
-    par->lthread = false;
   }
   else // insert as right child
   {
-    node->lthread = node->rthread = true;
-    node->right = par->right;
-    node->left = par;
     par->right = node;
-    par->rthread = false;
   }
 }
 
 // search element in bst
-Node *BST::search(int x)
+bool BST::search(int x)
 {
   Node *cur = root;
 
@@ -170,41 +140,28 @@ Node *BST::search(int x)
   {
     // return if found
     if (x == cur->val)
-      return cur;
+      return true;
 
     else if (x < cur->val)
     {
-      // go to left child if exists
-      if (!cur->lthread)
-        cur = cur->left;
-      else
-        break;
+      // go to left child
+      cur = cur->left;
     }
     else
     {
-      // go to right child if exists
-      if (!cur->rthread)
-        cur = cur->right;
-      else
-        break;
+      // go to right child
+      cur = cur->right;
     }
   }
 
   // not found
-  return NULL;
+  return false;
 }
 
 // deletekey function
 void BST::deleteKey(int x)
 {
   Node *cur = root, *par = NULL;
-
-  // if element to be deleted not present in
-  // the bst then throw an exception
-  // if (!search(x))
-  // {
-  //   throw -1;
-  // }
 
   // find the element to be deleted
   while (cur)
@@ -214,206 +171,219 @@ void BST::deleteKey(int x)
     {
       break;
     }
-    else if (x < cur->val)
+
+    par = cur;
+    if (x < cur->val)
     {
-      par = cur;
       cur = cur->left;
     }
     else
     {
-      par = cur;
       cur = cur->right;
     }
   }
   if (!cur)
     return;
 
-  if (cur->lthread && cur->rthread) // cur is leaf node
+  // Check if the node to be
+  // deleted has atmost one child.
+  if (cur->left == NULL || cur->right == NULL)
   {
-    deleteLeaf(cur, par);
+
+    // newcur will replace
+    // the node to be deleted.
+    Node *newcur;
+
+    // if the left child does not exist.
+    if (cur->left == NULL)
+      newcur = cur->right;
+    else
+      newcur = cur->left;
+
+    // check if the node to
+    // be deleted is the root.
+    if (par == NULL)
+    {
+      root = NULL;
+      delete (newcur);
+    }
+
+    // check if the node to be deleted
+    // is par's left or right child
+    // and then replace this with newcur
+    if (cur == par->left)
+      par->left = newcur;
+    else
+      par->right = newcur;
+
+    // free memory of the
+    // node to be deleted.
+    delete (cur);
   }
-  else if (!cur->lthread && !cur->rthread) // cur has two children
+
+  // node to be deleted has
+  // two children.
+  else
   {
-    deleteNodeWithTwoChildren(cur, par);
-  }
-  else // cur has one child
-  {
-    deleteNodeWithOneChild(cur, par);
+    Node *p = NULL;
+    Node *temp;
+
+    // Compute the inorder successor
+    temp = cur->right;
+    while (temp->left != NULL)
+    {
+      p = temp;
+      temp = temp->left;
+    }
+
+    // check if the parent of the inorder
+    // successor is the cur or not(i.e. cur=
+    // the node which has the same data as
+    // the given data by the user to be
+    // deleted). if it isn't, then make the
+    // the left child of its parent equal to
+    // the inorder successor'd right child.
+    if (p != NULL)
+      p->left = temp->right;
+
+    // if the inorder successor was the
+    // cur (i.e. cur = the node which has the
+    // same data as the given data by the
+    // user to be deleted), then make the
+    // right child of the node to be
+    // deleted equal to the right child of
+    // the inorder successor.
+    else
+      cur->right = temp->right;
+
+    cur->val = temp->val;
+    delete (temp);
   }
 }
 
-void BST::deleteLeaf(Node *node, Node *par)
-{
-  // If Node to be deleted is root
-  if (!par)
-    root = NULL;
+// void BST::deleteLeaf(Node *node, Node *par)
+// {
+//   // If Node to be deleted is root
+//   if (!par)
+//     root = NULL;
 
-  // If Node to be deleted is left
-  // of its parent
-  else if (node == par->left)
-  {
-    par->lthread = true;
-    par->left = node->left;
-  }
-  else
-  {
-    par->rthread = true;
-    par->right = node->right;
-  }
+//   // If Node to be deleted is left
+//   // of its parent
+//   else if (node == par->left)
+//   {
+//     par->left = NULL;
+//   }
+//   else
+//   {
+//     par->right = NULL;
+//   }
 
-  // Free memory
-  delete (node);
-}
+//   // Free memory
+//   delete (node);
+// }
 
-void BST::deleteNodeWithTwoChildren(Node *node, Node *par)
-{
-  // Find inorder successor and its parent.
-  Node *parsucc = node;
-  Node *succ = node->right;
+// void BST::deleteNodeWithTwoChildren(Node *node, Node *par)
+// {
+//   // Find inorder successor and its parent.
+//   Node *parsucc = node;
+//   Node *succ = node->right;
 
-  // Find leftmost child of successor
-  while (!succ->lthread && succ->left != NULL)
-  {
-    parsucc = succ;
-    succ = succ->left;
-  }
+//   // Find leftmost child of successor
+//   while (succ->left != NULL)
+//   {
+//     parsucc = succ;
+//     succ = succ->left;
+//   }
 
-  node->val = succ->val;
+//   node->val = succ->val;
 
-  if (succ->lthread == true && succ->rthread == true)
-    deleteLeaf(succ, parsucc);
-  else
-    deleteNodeWithOneChild(succ, parsucc);
-}
+//   if (!succ->left && !succ->right)
+//     deleteLeaf(succ, parsucc);
+//   else
+//     deleteNodeWithOneChild(succ, parsucc);
+// }
 
-void BST::deleteNodeWithOneChild(Node *node, Node *par)
-{
-  Node *child;
+// void BST::deleteNodeWithOneChild(Node *node, Node *par)
+// {
+//   Node *child;
 
-  // Initialize child Node to be deleted has
-  // left child.
-  if (node->lthread == false)
-    child = node->left;
+//   // Initialize child Node to be deleted has
+//   // left child.
+//   if (node->left)
+//     child = node->left;
 
-  // Node to be deleted has right child.
-  else
-    child = node->right;
+//   // Node to be deleted has right child.
+//   else
+//     child = node->right;
 
-  // Node to be deleted is root Node.
-  if (par == NULL)
-    root = child;
+//   // Node to be deleted is root Node.
+//   if (par == NULL)
+//     root = child;
 
-  // Node is left child of its parent.
-  else if (node == par->left)
-    par->left = child;
-  else
-    par->right = child;
+//   // Node is left child of its parent.
+//   else if (node == par->left)
+//     par->left = child;
+//   else
+//     par->right = child;
 
-  // Find successor and predecessor
-  Node *s = successor(node);
-  Node *p = predecessor(node);
+//   // Find successor and predecessor
+//   Node *s = successor(node);
+//   Node *p = predecessor(node);
 
-  // If node has left subtree.
-  if (node->lthread == false)
-    p->right = s;
+//   // If node has left subtree.
+//   if (node->left)
+//     p->right = s;
 
-  // If node has right subtree.
-  else
-  {
-    if (node->rthread == false)
-      s->left = p;
-  }
+//   // If node has right subtree.
+//   else
+//   {
+//     if (node->right)
+//       s->left = p;
+//   }
 
-  // free memory
-  delete (node);
-}
+//   // free memory
+//   delete (node);
+// }
 
 // find successor of a node
-Node *BST::successor(Node *node)
-{
-  Node *cur = node;
+// Node *BST::successor(Node *node)
+// {
+//   Node *cur = node;
 
-  // if right child does not exist
-  // return successor stored as right child
-  if (cur->rthread)
-    return cur->right;
+//   // if right child does not exist
+//   // return successor stored as right child
+//   if (cur->rthread)
+//     return cur->right;
 
-  // go to right child if exists
-  cur = cur->right;
-  if (!cur)
-    return cur;
+//   // go to right child if exists
+//   cur = cur->right;
+//   if (!cur)
+//     return cur;
 
-  // go to left most element
-  while (!cur->lthread && cur->left)
-    cur = cur->left;
-  return cur;
-}
+//   // go to left most element
+//   while (cur->left)
+//     cur = cur->left;
+//   return cur;
+// }
 
-Node *BST::predecessor(Node *node)
-{
-  // if left child does not exist
-  // return successor stored as left child
-  Node *cur = node;
-  if (cur->lthread)
-    return cur->left;
+// Node *BST::predecessor(Node *node)
+// {
+//   // if left child does not exist
+//   // return successor stored as left child
+//   Node *cur = node;
+//   if (cur->lthread)
+//     return cur->left;
 
-  // go to left child if exists
-  cur = cur->left;
-  if (!cur)
-    return cur;
+//   // go to left child if exists
+//   cur = cur->left;
+//   if (!cur)
+//     return cur;
 
-  // go to right most element
-  while (!cur->rthread && cur->right)
-    cur = cur->right;
-  return cur;
-}
-
-// function to print the bst
-void BST::printTree()
-{
-  ofstream fout;
-
-  fout.open("BST.dot");
-  fout << "digraph g {\n";
-  printTreeHelper(root, fout);
-  fout << "}";
-  fout.close();
-  system("dot -Tpng BST.dot -o BST.png");
-}
-
-void BST::printTreeHelper(const Node *node, ofstream &fout)
-{
-  if (!node)
-    return;
-
-  if (node == root) // add the label and root in the dot file
-  {
-    fout << "label = \" rooted at " << node->val << " \";\n";
-    fout << node->val << " [root = true]\n";
-  }
-
-  if (!node->lthread) // if left child exits
-  {
-    fout << node->val << " -> " << node->left->val << "\n";
-    printTreeHelper(node->left, fout);
-  }
-  else
-  {
-    if (node->left)
-      fout << node->val << " -> " << node->left->val << "[style = dotted];\n";
-  }
-
-  if (!node->rthread) // if right child exists
-  {
-    fout << node->val << " -> " << node->right->val << "\n";
-    printTreeHelper(node->right, fout);
-  }
-  else
-  {
-    if (node->right)
-      fout << node->val << " -> " << node->right->val << "[style = dotted];\n";
-  }
-}
+//   // go to right most element
+//   while (!cur->rthread && cur->right)
+//     cur = cur->right;
+//   return cur;
+// }
 
 //========================================================================================================================
 
@@ -428,8 +398,8 @@ int BST::BST_Height_Helper(Node *node)
 {
   if (!node)
     return 0;
-  int lh = node->lthread ? 0 : BST_Height_Helper(node->left);
-  int rh = node->rthread ? 0 : BST_Height_Helper(node->right);
+  int lh = BST_Height_Helper(node->left);
+  int rh = BST_Height_Helper(node->right);
   return 1 + max(lh, rh);
 }
 
@@ -451,8 +421,8 @@ long int BST::BST_Average_Height_Of_Nodes_Helper(Node *node, long int &total_hei
   if (!node)
     return 0;
   ++number_of_nodes;
-  long int lh = node->lthread ? 0 : BST_Average_Height_Of_Nodes_Helper(node->left, total_height, number_of_nodes);
-  long int rh = node->rthread ? 0 : BST_Average_Height_Of_Nodes_Helper(node->right, total_height, number_of_nodes);
+  long int lh = BST_Average_Height_Of_Nodes_Helper(node->left, total_height, number_of_nodes);
+  long int rh = BST_Average_Height_Of_Nodes_Helper(node->right, total_height, number_of_nodes);
 
   int h = max(lh, rh) + 1;
   total_height += h;
@@ -460,3 +430,8 @@ long int BST::BST_Average_Height_Of_Nodes_Helper(Node *node, long int &total_hei
 }
 
 //========================================================================================================================
+
+int BST::get_number_of_rotations_during_insertion() { return number_of_rotations_during_insertion; }
+int BST::get_number_of_rotations_during_deletion() { return number_of_rotations_during_deletion; }
+int BST::get_number_of_comparisons_during_insertion() { return number_of_comparisons_during_insertion; }
+int BST::get_number_of_comparisons_during_deletion() { return number_of_comparisons_during_deletion; }
